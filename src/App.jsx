d9bfab76ddb99b97370dd8fdd4c84dcfe4b10515
kgisellee <kgiselleaguirrelara@gmail.com>
2024-02-10 16:02:33 -0600

    maquetado

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
 import './App.css'
-
-import './App.css'
-
+import escudo from './images/escudo.jpg'
 function App() {
   
-
   return (
-    <>
-      <h1>Hola mundo</h1>
-    </>
+  <>
+    <div class='header'>
+      <img src={escudo} class='esc'/>
+      <h1>LISTA DE TAREAS DE LA PLANEACION</h1>
+    </div>
+    <div class='centrar'>
+      <input class='input' placeholder='escribe tu nueva tarea'/>
+      <button class='butti'>Nueva Tarea</button>
+    </div>
+    <div class='centrar'>
+    <ul class='tareas'>
+        <li>
+          tarea 1
+        </li>
+        <li>
+          tarea 2
+        </li>
+        <li>
+          tarea 3
+        </li>
+      </ul>
+    </div>
+
+  </>  
   )
 }
 
